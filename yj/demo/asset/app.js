fe4800a052fa443454880f6f0d4105a4fb9292fa
Khoo Yong Jun <khoo.jack@gmail.com>
2017-11-30 17:37:02 +0800

    demo

--- yj/demo/asset/app.js
+++ yj/demo/asset/app.js
@@ -6,3 +6,10 @@ for (var i = 0; i < 5; i++) {
 	document.write('<br>');
 }
 
+input = '';
+sum = 0;
+while (input != 'x') {
+	input = prompt('Please enter a number to add to me. (x to exit)\nNow the sum is ' + sum);
+	inputNumber = parseInt(input);
+	if(input !='x') if(!isNaN(inputNumber)) sum += inputNumber;
+}
